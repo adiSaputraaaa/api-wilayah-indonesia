4596b78b4991a8260a637fb958c47e951050c207
azaleaiguana <adi.fernandez25@gmail.com>
2025-05-30 03:53:24 +0700

    refactor: simplify data retrieval by importing provinces directly from JSON

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,7 @@
-import fs from "fs";
-import path from "path";
+import provinces from "./provinces.json";
 
 export default function handler(req, res) {
-  const filePath = path.resolve("./api", "provinces.json");
-  const fileData = fs.readFileSync(filePath, "utf8");
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
-  res.status(200).end(fileData);
+  res.status(200).json(provinces);
 }
