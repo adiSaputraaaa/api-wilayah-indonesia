d4fa75fc26c3d419c665e416fd5603fec2e08a88
azaleaiguana <adi.fernandez25@gmail.com>
2025-05-30 03:27:31 +0700

    6

---
+++ api/provinces.ts
@@ -0,0 +1,7 @@
+import provinces from "./provinces.json";
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(provinces);
+}
