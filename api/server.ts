052287249cf02eff2591a82e42cc14abed2dc967
azaleaiguana <adi.fernandez25@gmail.com>
2025-05-30 03:50:17 +0700

    9

--- api/server.ts
+++ api/server.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const filePath = path.resolve("./static/api", "provinces.json");
+  const filePath = path.resolve("./api", "provinces.json");
   const fileData = fs.readFileSync(filePath, "utf8");
 
   res.setHeader("Access-Control-Allow-Origin", "*");
