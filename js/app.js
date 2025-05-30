b9507f200b8e441cef881d7479f78678003bbc27
azaleaiguana <adi.fernandez25@gmail.com>
2025-05-30 11:56:52 +0700

    1

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ function getCurrentUrl() {
   return url;
 }
 
+// eslint-disable-next-line no-unused-vars
 const el = new Vue({
   el: "#app",
   data: {
@@ -136,7 +137,7 @@ const el = new Vue({
   methods: {
     async fetchProvinces() {
       this.fetchingProvinces = true;
-      const result = await fetch(`data/provinces.json`);
+      const result = await fetch(`/provinces.json`);
       this.fetchingProvinces = false;
       this.provinces = await result.json();
     },
@@ -146,7 +147,7 @@ const el = new Vue({
         return;
       }
       this.fetchingRegencies = true;
-      const result = await fetch(`data/regencies/${this.provinceId}.json`);
+      const result = await fetch(`/regencies/${this.provinceId}.json`);
       this.fetchingRegencies = false;
       this.regencies = await result.json();
     },
@@ -157,7 +158,7 @@ const el = new Vue({
       }
 
       this.fetchingDistricts = true;
-      const result = await fetch(`data/districts/${this.regencyId}.json`);
+      const result = await fetch(`/districts/${this.regencyId}.json`);
       this.fetchingDistricts = false;
       this.districts = await result.json();
     },
@@ -168,7 +169,7 @@ const el = new Vue({
       }
 
       this.fetchingVillages = true;
-      const result = await fetch(`data/villages/${this.districtId}.json`);
+      const result = await fetch(`/villages/${this.districtId}.json`);
       this.fetchingVillages = false;
       this.villages = await result.json();
     },
